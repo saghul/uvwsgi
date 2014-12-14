d2f7030f6f0a474ad710432f82659ecc628a3b8e
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-12-14 10:51:47 +0100

    Updated classifiers

--- setup.py
+++ setup.py
@@ -33,13 +33,10 @@ setup(name             = "uvwsgi",
           "Operating System :: Microsoft :: Windows",
           "Programming Language :: Python",
           "Programming Language :: Python :: 2",
-          "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.0",
-          "Programming Language :: Python :: 3.1",
-          "Programming Language :: Python :: 3.2",
-          "Programming Language :: Python :: 3.3"
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4"
                          ]
 )
 
